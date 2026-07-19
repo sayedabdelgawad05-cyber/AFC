1c5234775d51fb79c2021e00a30e3a323cf3697b
Abdelgawad <sayed.abdelgawad@siemens.com>
2026-07-19 10:47:05 +0300

    Fix render startup

--- server.ts
+++ server.ts
@@ -1,15 +1,13 @@
 import express from 'express';
 import path from 'path';
 import fs from 'fs';
-import { fileURLToPath } from 'url';
 import { GoogleGenAI, Type } from '@google/genai';
 import dotenv from 'dotenv';
 import { DEFAULT_USERS, DEFAULT_STATIONS, DEFAULT_RFIS, DEFAULT_NCRS, DEFAULT_PUNCH_ITEMS } from './src/defaultData.js';
 
 dotenv.config();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+
 
 const app = express();
 const PORT = 3000;
