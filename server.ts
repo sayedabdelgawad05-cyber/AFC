48be373908e6447076fe89792884d7747e93a3ea
Abdelgawad <sayed.abdelgawad@siemens.com>
2026-07-20 11:39:15 +0300

    Debug login

--- server.ts
+++ server.ts
@@ -119,6 +119,9 @@ app.post('/api/auth/login', (req, res) => {
  
 const { username, password } = req.body;
 
+console.log('LOGIN REQUEST:', username, password);
+console.log('USERS IN DB:', db.users);
+
 const user = db.users.find(
   u =>
     u.username === username &&
